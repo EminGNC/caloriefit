0223d72be965e3d98dbf1fe46f14d2b21dc981cd
EminGNC <ilkergenc131@gmail.com>
2026-04-25 19:55:33 +0300

    initial commit

---
+++ src/main/java/com/caloriefit/caloriefit/CaloriefitApplication.java
@@ -0,0 +1,13 @@
+package com.caloriefit.caloriefit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaloriefitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaloriefitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caloriefit/caloriefit/CaloriefitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caloriefit.caloriefit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloriefitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
